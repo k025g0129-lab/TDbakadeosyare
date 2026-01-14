c71ef73094be271e59bd386f307e22fdd108d57d
ShibuyaRyosuke <k025g0008@g.neec.ac.jp>
2026-01-14 15:05:23 +0900

    基礎的な要素だけでとりあえずobjectを作った

--- Object.h
+++ Object.h
@@ -1,4 +1,17 @@
 ﻿#pragma once
+#include"Vector2.h"
+
 class Object {
+public:
+	Vector2 position;
+	float height;
+	float width;
+	bool isActive;
+
+	Object(Vector2 initialPosition);
+	~Object();
+
+	void Update();
+	void Draw();
 };
 
